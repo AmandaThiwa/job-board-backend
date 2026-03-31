2b159c99a8d8589c8a2a41883c1ebf8cde3b4f9f
AmandaThiwa <153912255+AmandaThiwa@users.noreply.github.com>
2026-03-31 18:00:55 +0530

    completed interview service swagger implementation

--- interview-service/index.js
+++ interview-service/index.js
@@ -4,7 +4,9 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import connectDB from './config/db.js';
 import interviewRoutes from './routes/interviewRoutes.js';
-
+import swaggerUi from 'swagger-ui-express';
+import fs from 'fs';
+import path from 'path';
 import dns from "node:dns/promises";
 dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
@@ -17,7 +19,10 @@ const PORT = process.env.PORT || 3004;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ origin: true, credentials: true }));
-
+const swaggerDocument = JSON.parse(
+    fs.readFileSync(path.resolve('./swagger.json'), 'utf-8')
+);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api/interviews', interviewRoutes);
 
 app.listen(PORT, () => {
